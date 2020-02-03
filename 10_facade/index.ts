5290b0d5baa967f86e1cfa4df44ab8a316baa95a
KirVorobiov <kir.vorobiov@cloudbeds.com>
2020-02-03 10:30:14 +0200

    facade implementation

---
+++ 10_facade/index.ts
@@ -0,0 +1,26 @@
+import { ICalculator } from "./ICalculator";
+import { CalculatorFacade } from "./CalculatorFacade";
+
+class Main {
+  async demo() {
+    const calculator: ICalculator = new CalculatorFacade();
+
+    const firstNumber = 20;
+    const secondNumber = 5;
+
+    const sumResult = await calculator.sum(firstNumber, secondNumber);
+    console.assert(sumResult === (firstNumber + secondNumber), 'Sum error!');
+
+    const subtractResult = await calculator.substract(firstNumber, secondNumber);
+    console.assert(subtractResult === (firstNumber - secondNumber), 'Subtract error!')
+    
+    const divideResult = await calculator.divide(firstNumber, secondNumber);
+    console.assert(divideResult === (firstNumber / secondNumber), 'Divide error!')
+    
+    const multiplyResult = await calculator.multiply(firstNumber, secondNumber);
+    console.assert(multiplyResult === (firstNumber * secondNumber), 'Multiply error!')
+  }
+}
+
+const app = new Main();
+app.demo();
