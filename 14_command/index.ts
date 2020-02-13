af216a757b4efdd04f94cbc18a7a50ef8247a1d4
KirVorobiov <kir.vorobiov@cloudbeds.com>
2020-02-13 23:41:11 +0200

    improve test cases

--- 14_command/index.ts
+++ 14_command/index.ts
@@ -9,42 +9,42 @@ class Main {
     const pizza = new Pizza();
 
     let ingridients = pizza.getIngridients();
-    console.assert(!ingridients.bacon, 'something goes wrong!');
-    console.assert(!ingridients.cheese, 'something goes wrong!');
-    console.assert(!ingridients.mashroom, 'something goes wrong!');
-    console.assert(!ingridients.pineapple, 'something goes wrong!');
-    console.assert(!ingridients.seafood, 'something goes wrong!');
+    console.assert(ingridients.bacon === false, 'something goes wrong!');
+    console.assert(ingridients.cheese === false, 'something goes wrong!');
+    console.assert(ingridients.mashroom === false, 'something goes wrong!');
+    console.assert(ingridients.pineapple === false, 'something goes wrong!');
+    console.assert(ingridients.seafood === false, 'something goes wrong!');
 
     /* ADD BACON AND CHEESE TO PIZZA */
     const pizzaInvoker = new PizzaInvoker();
-    pizzaInvoker.addIngridients(pizza, [Ingridients.bacon, Ingridients.cheese]);
+    pizzaInvoker.addIngridients(pizza, [Ingridients.bacon, Ingridients.pineapple ,Ingridients.cheese]);
 
     ingridients = pizza.getIngridients();
-    console.assert(ingridients.bacon, 'something goes wrong!');
-    console.assert(ingridients.cheese, 'something goes wrong!');
-    console.assert(!ingridients.mashroom, 'something goes wrong!');
-    console.assert(!ingridients.pineapple, 'something goes wrong!');
-    console.assert(!ingridients.seafood, 'something goes wrong!');
+    console.assert(ingridients.bacon === true, 'something goes wrong!');
+    console.assert(ingridients.cheese === true, 'something goes wrong!');
+    console.assert(ingridients.mashroom === false, 'something goes wrong!');
+    console.assert(ingridients.pineapple === true, 'something goes wrong!');
+    console.assert(ingridients.seafood === false, 'something goes wrong!');
 
     /* UNDO ADDING CHEESE TO PIZZA */
     pizzaInvoker.undoLast();
 
     ingridients = pizza.getIngridients();
-    console.assert(ingridients.bacon, 'something goes wrong!');
-    console.assert(!ingridients.cheese, 'something goes wrong!');
-    console.assert(!ingridients.mashroom, 'something goes wrong!');
-    console.assert(!ingridients.pineapple, 'something goes wrong!');
-    console.assert(!ingridients.seafood, 'something goes wrong!');
+    console.assert(ingridients.bacon === true, 'something goes wrong!');
+    console.assert(ingridients.cheese === false, 'something goes wrong!');
+    console.assert(ingridients.mashroom === false, 'something goes wrong!');
+    console.assert(ingridients.pineapple === true, 'something goes wrong!');
+    console.assert(ingridients.seafood === false, 'something goes wrong!');
 
     /* REDO LAST UNDO ACTION (adding cheese) */
     pizzaInvoker.redoLast();
 
     ingridients = pizza.getIngridients();
-    console.assert(ingridients.bacon, 'something goes wrong!');
-    console.assert(ingridients.cheese, 'something goes wrong!');
-    console.assert(!ingridients.mashroom, 'something goes wrong!');
-    console.assert(!ingridients.pineapple, 'something goes wrong!');
-    console.assert(!ingridients.seafood, 'something goes wrong!');
+    console.assert(ingridients.bacon === true, 'something goes wrong!');
+    console.assert(ingridients.cheese === true, 'something goes wrong!');
+    console.assert(ingridients.mashroom === false, 'something goes wrong!');
+    console.assert(ingridients.pineapple === true, 'something goes wrong!');
+    console.assert(ingridients.seafood === false, 'something goes wrong!');
   }
 }
 
